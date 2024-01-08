6d7b4f2b4d58311707ec7024d25339e1245576c0
Fayiz-hdsb <1alifay@hdsb.ca>
2024-01-08 10:16:03 -0500

    backend complete without login code

---
+++ main.py
@@ -0,0 +1,162 @@
+'''
+Author: Fayiz Ali
+Date created: Jan 7, 2024
+'''
+
+#refereced syntax from https://flask.palletsprojects.com/en/3.0.x/quickstart/, implemented on my own
+#https://medium.com/google-cloud/building-a-flask-python-crud-api-with-cloud-firestore-firebase-and-deploying-on-cloud-run-29a10c502877 
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, auth, exceptions, initialize_app
+
+app = Flask(__name__) #initialize the main central object
+
+#initialize firebase
+cred = credentials.Certificate('./config/key.json')
+firebaseApp = initialize_app(cred)
+
+#initialize firestore
+db = firestore.client()
+
+approvedListingsRef = db.collection('ApprovedListings')
+unapprovedListingsRef = db.collection('UnapprovedListings')
+
+#initialize firebase authentication
+firebase_auth:auth.Client = auth.Client(firebaseApp)
+
+successDict = {
+    'status': 200
+}
+
+errorDict = {
+    'status': 500
+}
+
+incompleteDict = {
+    'status': 422
+}
+
+unauthorizedDict = {
+    'status': 403
+}
+
+ADMIN_TOKEN:str = 'NtoRDfiE7vXf1YRKdC3vWy1on9X2'
+
+#https://stackoverflow.com/questions/14993318/catching-a-500-server-error-in-flask
+@app.errorhandler(500)
+def handleError(error):
+    return errorDict
+
+@app.route("/")
+def sendDefault():
+    return "<h2>Home Page</h2>"
+
+@app.route("/login", methods=['POST'])
+def login():
+    return "<h2>As Salaamu Alaikum</h2>"
+
+#https://firebase.google.com/docs/reference/admin/python/firebase_admin.auth 
+@app.route("/sign-up",  methods=['POST'])
+def signUp():
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if(email==None or password==None):
+            return incompleteDict
+
+        res = firebase_auth.create_user(email=email, password=password)
+        if(res):
+            return successDict
+        
+    except exceptions.FirebaseError as e:
+        print('Error occurred while creating user FirebaseError: {e}')
+        if(type(e) is auth.EmailAlreadyExistsError):
+            return {
+                'status': 409
+            }
+        else:
+            raise Exception(e)     
+    except ValueError as e:
+        print('Error occurred while creating user ValueError: {e}')
+        raise Exception(e)
+    except Exception as e:
+        print(f'Exception occurred while creating user: {e}')
+        raise Exception(e)
+
+@app.route("/create-connection", methods=['POST'])
+def createConnection():
+    try:
+        title = request.form.get('title')
+        description = request.form.get('description')
+        authorId = request.form.get('id')
+        type = request.form.get('type')
+        link = request.form.get('link')
+        location = request.form.get('location')
+
+        responseDict = {
+            'title': title,
+            'description': description,
+            'authorId': authorId,
+            'type': type,
+            'link': link,
+            'location': location,
+        }
+
+        if(title == None or description == None or authorId == None or type == None or location == None):
+            return incompleteDict
+
+        unapprovedListingsRef.document().create(responseDict)
+
+        return successDict
+    except Exception as e:
+        print(f"An error occurred while creating connection: {e}")
+        raise Exception(e)
+
+@app.route("/approve-connection", methods=['POST'])
+def approveConnection():
+    try:
+        listingId = None
+        
+        if(checkIfHasAdminAccess()):
+            listingId = request.form.get('listingId')
+            unapprovedDocument = unapprovedListingsRef.document(listingId).get()
+
+            approvedListingsRef.document(listingId).create((unapprovedDocument.to_dict()))
+
+            unapprovedListingsRef.document(listingId).delete()
+
+            return successDict
+        else:
+            return unauthorizedDict
+    except Exception as e:
+        print(f"An error occurred while approving connection with id {listingId} : {e}")
+        raise Exception(e)
+
+@app.route("/get-connections", methods=['GET'])
+def getConnections():
+    try:
+        shouldFetchUnapprovedRequests:bool = (request.args.get('unapproved') == 'true')
+
+        if(shouldFetchUnapprovedRequests):
+            if(checkIfHasAdminAccess()):
+                docs = unapprovedListingsRef.stream()
+            else:
+                return unauthorizedDict
+        else:
+            docs = approvedListingsRef.stream()
+
+        allListings = []
+        for doc in docs:
+            modifiedDoc = doc.to_dict()
+            modifiedDoc['id'] = doc.id
+            
+            allListings.append(modifiedDoc)
+
+        return allListings
+    except Exception as e:
+        print(f"An error occurred while getting connections: {e}")
+        raise Exception(e)
+    
+
+def checkIfHasAdminAccess() -> bool:
+    return (request.form.get('token') == ADMIN_TOKEN)
